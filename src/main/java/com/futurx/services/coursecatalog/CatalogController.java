775c0f2cbd29cccb9435b9d9ffbf7e39cb8a7d7e
Simon Vargas <sjvargas01@gmail.com>
2023-03-04 14:38:24 -0500

    updated catalogController to connect to courseApp through eureka client server

--- src/main/java/com/futurx/services/coursecatalog/CatalogController.java
+++ src/main/java/com/futurx/services/coursecatalog/CatalogController.java
@@ -2,7 +2,7 @@ package com.futurx.services.coursecatalog;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.sun.source.tree.InstanceOfTree;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,13 +10,14 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CatalogController {
 
-   private EurekaClient client;
+    @Autowired
+    private EurekaClient client;
 
     @RequestMapping("/")
     public String getCatalogHome() {
         String courseAppMessage = "";
         //String courseAppURL = "http://localhost:8080";
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("courseapp", false);
         RestTemplate restTemplate = new RestTemplate();
         String courseAppURL = instanceInfo.getHomePageUrl();
        courseAppMessage = restTemplate.getForObject(courseAppURL, String.class);
@@ -28,7 +29,7 @@ public class CatalogController {
     public String getCatalog() {
         String courses = "";
         //String courseAppURL = "http://localhost:8080/courses";
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("courseapp", false);
         RestTemplate restTemplate = new RestTemplate();
         String courseAppURL = instanceInfo.getHomePageUrl() + "/courses";
         courses = restTemplate.getForObject(courseAppURL, String.class);
@@ -40,7 +41,7 @@ public class CatalogController {
     public String getSpecificCourse() {
         Course course = new Course();
         //String courseAppURL = "http://localhost:8080/1";
-        InstanceInfo instanceInfo = client.getNextServerFromEureka("fx-course-service", false);
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("courseapp", false);
         String courseAppURL = instanceInfo.getHomePageUrl();
         courseAppURL = courseAppURL + "/1";
         RestTemplate restTemplate = new RestTemplate();
